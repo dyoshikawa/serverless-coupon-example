6ee79f0e94fed73a95e489c6e448ff291fc6f548
dyoshikawa <dyoshikawa1993@gmail.com>
2020-07-10 11:27:02 +0900

    タイムゾーン対応は保留

--- lambda-src/src/infrastructure/TimeImpl.ts
+++ lambda-src/src/infrastructure/TimeImpl.ts
@@ -1,12 +1,7 @@
 import dayjs from 'dayjs'
-import 'dayjs/locale/ja'
 import { Time } from '../time/Time'
 
 export class TimeImpl implements Time {
-  constructor(locale: 'utc' | 'ja' = 'ja') {
-    dayjs().locale(locale)
-  }
-
   now(): Date {
     return dayjs().toDate()
   }
